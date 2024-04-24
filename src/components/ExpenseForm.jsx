c329defa30caac280ab8ef50c4a987ae45392d3a
Rakesh60 <rakeshgupta5353@gmail.com>
2024-04-25 00:58:27 +0530

    validation Rules implemented

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -7,24 +7,56 @@ function ExpenseForm({ setExpense }) {
     title: "",
     category: "",
     amount: "",
+    // email: "",
   });
   const [errors, setErrors] = useState({});
+  //* Validation Function
+  const ValidateRules = {
+    title: [
+      { required: true, message: "Please enter title" },
+      { minLength: 5, message: "Title Should be at least 5 character" },
+    ],
+    category: [{ required: true, message: "Please Select a category" }],
+    amount: [
+      { required: true, message: "Please enter an amount" },
+      { isNumber: true, message: "Please enter only numbers" },
+    ],
+    // email: [
+    //   { required: true, message: "Please enter an Email" },
+    //   {
+    //     pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    //     message: "Please enter a valid Email",
+    //   },
+    // ],
+  };
+
   const validate = (validateForm) => {
     const errorData = {};
-    if (!validateForm.title) {
-      errorData.title = "title is Required";
-    }
-    if (!validateForm.category) {
-      errorData.category = "please select a category";
-    }
-    if (!validateForm.amount) {
-      errorData.amount = "amount is Required";
-    }
+    Object.entries(validateForm).forEach(([key, value]) => {
+      ValidateRules[key].some((rule) => {
+        // console.log(rule);
+        if (rule.required && !value) {
+          errorData[key] = rule.message;
+          return true;
+        }
+        if (rule.minLength && value.length < 5) {
+          errorData[key] = rule.message;
+          return true;
+        }
+        if (rule.isNumber && isNaN(value)) {
+          errorData[key] = rule.message;
+          return true;
+        }
+        // if (rule.pattern && !rule.pattern.test(value)) {
+        //   errorData[key] = rule.message;
+        //   return true;
+        // }
+      });
+    });
+
     setErrors(errorData);
     return errorData;
   };
-
-
 
   //?Submitting Form Data
   const handleSubmit = (e) => {
@@ -32,7 +64,7 @@ function ExpenseForm({ setExpense }) {
     // const expense={title,category,amount,id:crypto.randomUUID()}
     const validateRes = validate(formData);
 
-    //*Agar Error me ek bhi length hai toh submit nhi hoga 
+    //*Agar Error me ek bhi length hai toh submit nhi hoga
     if (Object.keys(validateRes).length) return;
     setExpense((previousExpense) => [
       ...previousExpense,
@@ -42,18 +74,14 @@ function ExpenseForm({ setExpense }) {
       title: "",
       category: "",
       amount: "",
-    })
+    });
   };
-
 
   //?When User type Input unidirectional data
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
-
-
-
 
   return (
     <>
@@ -70,13 +98,13 @@ function ExpenseForm({ setExpense }) {
 
         <Select
           id={"category"}
-          label={'Category'}
+          label={"Category"}
           name={"category"}
           value={formData.category}
           onChange={handleChange}
           options={["grocery", "clothes", "bills", "education", "medicine"]}
           error={errors.category}
-          defaultOption={'Select a Category'}
+          defaultOption={"Select a Category"}
         />
         <Input
           label={"Amount"}
@@ -86,6 +114,14 @@ function ExpenseForm({ setExpense }) {
           onChange={handleChange}
           error={errors.amount}
         />
+        {/* <Input
+          label={"Email"}
+          id={"email"}
+          name={"email"}
+          value={formData.email}
+          onChange={handleChange}
+          error={errors.email}
+        /> */}
         <button className="add-btn">Add</button>
       </form>
     </>
